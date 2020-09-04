8d615ac4f7154d41ff434c83d67293270a29e108
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-09-04 16:10:36 +0300

    responsive

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,19 @@
 import 'normalize.css';
 import {inputForm} from "./input";
-import  scrolled from "./scrolled";
+import "./scrolled";
+
 
 
 $(document).ready(() => {
     $('input, textarea').on('input', inputForm);
+
+    $('.btn_filter').on('click', function (){
+        $(this).children('img').toggleClass('active');
+    })
 });
 
 $(window).resize(function () {
 
 });
+
+
